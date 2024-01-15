3adc2a827a98f35708aea2af7f2f4cc273de7064
jaycyisme <quoctuanisme0610@gmail.com>
2024-01-15 14:22:51 +0700

    Add Service Page

---
+++ category/Service/assets/js/custom.js
@@ -0,0 +1,14 @@
+$(".accordion-item .heading").on("click", function (e) {
+    e.preventDefault();
+
+    if ($(this).closest(".accordion-item").hasClass("active")) {
+        $(".accordion-item").removeClass("active");
+    } else {
+        $(".accordion-item").removeClass("active");
+
+        $(this).closest(".accordion-item").addClass("active");
+    }
+    var $content = $(this).next();
+    $content.slideToggle(100);
+    $(".accordion-item .content").not($content).slideUp("fast");
+});
