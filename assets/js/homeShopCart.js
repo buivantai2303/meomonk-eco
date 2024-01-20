0e455b0f3174737c6e8bed0a31629de26924c93e
jaycyisme <quoctuanisme0610@gmail.com>
2024-01-20 22:48:52 +0700

    Update GUI

--- assets/js/homeShopCart.js
+++ assets/js/homeShopCart.js
@@ -1,31 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-   
     const tableBody = document.querySelector(".shop_table tbody");
-    var cartList = JSON.parse(localStorage.getItem('cartPoPup'));
-    var totalElement = document.querySelector('.font-semi-bold.total');
+    var cartList = JSON.parse(localStorage.getItem("cartPoPup"));
+    var totalElement = document.querySelector(".font-semi-bold.total");
     console.log(totalElement);
-    var tongTien=0;
-        for (var i = 0; i < cartList.length; i++) {
+    var tongTien = 0;
+    for (var i = 0; i < cartList.length; i++) {
         // Call the function to append a new row with the given product
         // appendTableRow(cartList[i]);
-        addCartItem(cartList[i])    ; 
+        addCartItem(cartList[i]);
         tongTien += cartList[i].soLuong * cartList[i].priceNew;
-    };
+    }
     console.log = tongTien;
-    totalElement.innerHTML ="";
-    totalElement.innerHTML =tongTien + "đ";
-
+    totalElement.innerHTML = "";
+    totalElement.innerHTML = tongTien + "đ";
 
     function addCartItem(product) {
+        var cartList = document.querySelector(".cart-popup ul");
 
-    var cartList = document.querySelector('.cart-popup ul');
+        var li = document.createElement("li");
+        li.className = "d-flex align-items-center position-relative";
 
-
-    var li = document.createElement('li');
-    li.className = 'd-flex align-items-center position-relative';
-
-    li.innerHTML = `
+        li.innerHTML = `
         <div class="p-img light-bg">
             <img src="${product.image}" alt="${product.nameProduce}" />
         </div>
@@ -38,8 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
     `;
 
-    // Append the new list item to the cart list
-    cartList.appendChild(li);
-}
-}
-);
+        // Append the new list item to the cart list
+        cartList.appendChild(li);
+    }
+});
